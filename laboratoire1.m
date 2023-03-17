aa573b53f68213d8dc4a80568913eaefea4b056a
Giuseppe-Joey <giuseppe.lomonaco@usherbrooke.ca>
2023-03-17 11:55:40 -0400

    renaming laboratoire files

--- laboratoire1.m
+++ laboratoire1.m
@@ -108,3 +108,51 @@ plot(real(pol), imag(pol),'s')
 
 
 
+
+
+%% probleme 7
+err = 0;
+ts_max = 0.1;
+Mp = 5;
+
+num = [1 1];
+den1 = [1 6 10];
+den2 = [1 7];
+den = conv(den1,den2);
+
+G = tf(num,den);
+
+zPD = 0;
+
+
+
+%% probleme 8
+
+
+
+
+
+
+
+%% probleme 9
+
+
+
+
+
+%% probleme 10
+
+
+
+
+
+
+%% probleme 11
+
+
+
+
+
+
+
+
